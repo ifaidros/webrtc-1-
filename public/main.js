1a52abe5574a16ab7964d072e6718e3dd53c0e5e
ifaidros <ifaidros@gmail.com>
2020-02-28 16:33:48 +0000

    ICE changes

--- public/main.js
+++ public/main.js
@@ -9,10 +9,10 @@ let config = ''
 socket.on('onicecandidate', handIncoming)
 
 function handIncoming(message) {
-  console.log('****Received from server onicecandidate', message) 
+  console.log('-->Received from server onicecandidate', message) 
 
   let newcandidate = new RTCIceCandidate(message);
-  console.log('*************newcandidate ', newcandidate)
+  console.log('-->newcandidate ', newcandidate)
 
   myPeerConnection.addIceCandidate(newcandidate)
     .catch((reportError) => {
@@ -36,7 +36,7 @@ function handOffer(message) {
 
   if (!myPeerConnection) {
     createPeerConnection();
-    console.log('@@@@@@@@@@@@@@@@ DID NOT EXIST AND CREATED')
+    console.log('-->RTCPeerConnection DID NOT EXIST AND CREATED')
   }
 
   let desc = new RTCSessionDescription(message);
@@ -67,12 +67,12 @@ function handOffer(message) {
 socket.on('answer', handAnswer)
 
 function handAnswer(message) {
-  console.log('****Received answer', message) 
+  console.log('-->Received answer', message) 
 
   let desc = new RTCSessionDescription(message);
 
   myPeerConnection.setRemoteDescription(desc).then(() => {
-    console.log('----> desc created as result from the socket onAnswer', desc)
+    console.log('-->desc created as result from the socket onAnswer', desc)
   })
   .catch(handleGetUserMediaError);
 
@@ -81,7 +81,7 @@ function handAnswer(message) {
 socket.on('config', handConfig)
 
 function handConfig(message) {
-  console.log('****Received config', message) 
+  console.log('-->Received config from peer', message) 
   
   if (!config) {
     config = message  
@@ -106,7 +106,7 @@ getTokenButton.addEventListener('click', getTokenButtonAction);
 
 function startButtonAction() {
 
-  console.log('*******************', config)
+  console.log('-->Start button clicked with config: ', config)
 
   createPeerConnection();
 
@@ -118,15 +118,15 @@ function startButtonAction() {
   navigator.mediaDevices.getUserMedia(mediaConstraints)
   .then((localStream) => {
     document.querySelector('#localVideo').srcObject = localStream;
-    console.log('****localstream is ', localStream.getTracks());
+    console.log('-->localstream is ', localStream.getTracks());
     localStream.getTracks().forEach(track => myPeerConnection.addTrack(track, localStream));
-    console.log('****myPeerConnection ', myPeerConnection);
+    console.log('-->myPeerConnection ', myPeerConnection);
   })
   .catch(handleGetUserMediaError);
 }
 
 function handleGetUserMediaError(e) {
-  console.log('****handleGetUserMediaError Error ', e)
+  console.log('-->*handleGetUserMediaError Error ', e)
 }
 
 function createPeerConnection() {
@@ -138,7 +138,7 @@ function createPeerConnection() {
 
   myPeerConnection = new RTCPeerConnection(config);
 
-  console.log('****myPeerConnection ', myPeerConnection)
+  console.log('-->myPeerConnection ', myPeerConnection)
 
   myPeerConnection.onicecandidate = iceCallback;
   myPeerConnection.ontrack = handleTrackEvent;
@@ -153,7 +153,7 @@ function createPeerConnection() {
 function callButtonAction() {
   myPeerConnection.createOffer()
   .then((offer) => {
-  console.log('****Offer ', offer)
+  console.log('-->Offer ', offer)
   return myPeerConnection.setLocalDescription(offer)
   })
   .then(() => {
@@ -190,23 +190,23 @@ function callButtonAction() {
 function iceCallback(e) {
   if (e.candidate) {
     socket.emit('onicecandidate', e.candidate)
-    console.log('****Candidate: ', e.candidate)
+    console.log('-->Candidate: ', e.candidate)
   }
 
 }
 
 function handleTrackEvent(e) {
-  console.log('**************TRACKS streaming ', e.streams)
+  console.log('-->TRACKS streaming ', e.streams)
   document.querySelector("#remoteVideo").srcObject = e.streams[0];
 }
 
 async function handleNegotiationNeededEvent(e) {
   //console.log('****handleNegotiationNeededEvent called: ', e)
 
-  console.log("*** Negotiation needed");
+  console.log("-->Negotiation needed");
 
   try {
-    console.log("---> Creating offer");
+    console.log("-->Creating offer");
     const offer = await myPeerConnection.createOffer();
 
     // If the connection hasn't yet achieved the "stable" state,
@@ -214,23 +214,23 @@ async function handleNegotiationNeededEvent(e) {
     // will be fired when the state stabilizes.
 
     if (myPeerConnection.signalingState != "stable") {
-      console.log("     -- The connection isn't stable yet; postponing...")
+      console.log("-->The connection isn't stable yet; postponing...")
       return;
     }
 
     // Establish the offer as the local peer's current
     // description.
 
-    console.log("---> Setting local description to the offer");
+    console.log("-->Setting local description to the offer");
     await myPeerConnection.setLocalDescription(offer);
 
     // Send the offer to the remote peer.
 
-    console.log("---> Sending the offer to the remote peer");
+    console.log("-->Sending the offer to the remote peer");
     socket.emit('offer', myPeerConnection.localDescription)
 
   } catch(err) {
-    console.log("*** The following error occurred while handling the negotiationneeded event:", err);
+    console.log("-->The following error occurred while handling the negotiationneeded event:", err);
 
   };
 
@@ -241,41 +241,132 @@ async function handleNegotiationNeededEvent(e) {
 }
 
 function handleRemoveTrackEvent(e) {
-  console.log('****handleRemoveTrackEvent called: ', e)
+  console.log('-->handleRemoveTrackEvent called: ', e)
 
 }
 
 function handleICEConnectionStateChangeEvent(e) {
-  console.log('****handleICEConnectionStateChangeEvent called: ', e)
+  console.log('-->handleICEConnectionStateChangeEvent called: ', e)
+
+  switch(myPeerConnection.iceConnectionState) {
+    case "new":
+      console.log('-->iceConnectionState: ', e.target.iceConnectionState)
+      break;
+    case "connecting":
+      console.log('-->iceConnectionState: ', e.target.iceConnectionState)
+      break;
+    case "connected":
+      console.log('-->iceConnectionState: ', e.target.iceConnectionState)
+      break;
+    case "closed":
+      console.log('-->iceConnectionState: ', e.target.iceConnectionState)
+      break;
+    case "failed":
+      console.log('-->iceConnectionState: ', e.target.iceConnectionState)
+      break;
+    case "disconnected":
+      console.log('-->iceConnectionState: ', e.target.iceConnectionState)
+      closeVideoCall();
+      break;
+  }
+}
+
+function closeVideoCall() {
+  var remoteVideo = document.querySelector("#remoteVideo")
+  var localVideo = document.querySelector('#localVideo')
+
+  if (myPeerConnection) {
+    myPeerConnection.ontrack = null;
+    myPeerConnection.onremovetrack = null;
+    myPeerConnection.onremovestream = null;
+    myPeerConnection.onicecandidate = null;
+    myPeerConnection.oniceconnectionstatechange = null;
+    myPeerConnection.onsignalingstatechange = null;
+    myPeerConnection.onicegatheringstatechange = null;
+    myPeerConnection.onnegotiationneeded = null;
+
+    if (remoteVideo.srcObject) {
+      remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+    }
+
+    if (localVideo.srcObject) {
+      localVideo.srcObject.getTracks().forEach(track => track.stop());
+    }
+
+    myPeerConnection.close();
+    myPeerConnection = null;
+  }
+
+  remoteVideo.removeAttribute("src");
+  remoteVideo.removeAttribute("srcObject");
+  localVideo.removeAttribute("src");
+  remoteVideo.removeAttribute("srcObject");
+
+  document.querySelector("#hangupButton").disabled = true;
+
 }
 
 function handleICEGatheringStateChangeEvent(e) {
-  console.log('****handleICEGatheringStateChangeEvent called: ', e)
+  console.log('-->handleICEGatheringStateChangeEvent called: ', e)
+
+  switch(myPeerConnection.iceGatheringState) {
+    case "new":
+      console.log('-->iceGatheringState: ', e.target.iceGatheringState)
+      break;
+    case "gathering":
+      console.log('-->iceGatheringState: ', e.target.iceGatheringState)
+      break;
+    case "complete":
+      console.log('-->iceGatheringState: ', e.target.iceGatheringState)
+      break;
+  }
 }
 
 function handleSignalingStateChangeEvent(e) {
-  console.log('****handleSignalingStateChangeEvent called: ', e)
+  console.log('-->handleSignalingStateChangeEvent called: ', e)
+
+  switch(myPeerConnection.signalingState) {
+    case "stable":
+      console.log('-->signalingState: ', e.target.signalingState)
+      break;
+    case "have-local-offer":
+      console.log('-->signalingState: ', e.target.signalingState)
+      break;
+    case "have-remote-offer":
+      console.log('-->signalingState: ', e.target.signalingState)
+      break;
+      case "have-local-pranswer":
+        console.log('-->signalingState: ', e.target.signalingState)
+        break;
+      case "have-remote-pranswer":
+        console.log('-->signalingState: ', e.target.signalingState)
+        break;
+      case "closed":
+        console.log('-->signalingState: ', e.target.signalingState)
+        break;
+  }
 
 }
 
 function hangupButtonnAction() {
-  console.log('close the connection')
+  console.log('-->close the connection')
+  closeVideoCall()
 
 
-  myPeerConnection.getStats(null).then(stats => {
-    var statsOutput = "";
+  // myPeerConnection.getStats(null).then(stats => {
+  //   var statsOutput = "";
  
-    stats.forEach(report => {
-      if (report.type === "inbound-rtp" && report.kind === "video") {
-        Object.keys(report).forEach(statName => {
-          statsOutput += `<strong>${statName}:</strong> ${report[statName]}<br>\n`;
-        });
-      }
-    });
+  //   stats.forEach(report => {
+  //     if (report.type === "inbound-rtp" && report.kind === "video") {
+  //       Object.keys(report).forEach(statName => {
+  //         statsOutput += `<strong>${statName}:</strong> ${report[statName]}<br>\n`;
+  //       });
+  //     }
+  //   });
     
-    document.querySelector("#statsP").innerHTML = statsOutput;
-    console.log(statsOutput)
-  });  
+  //   document.querySelector("#statsP").innerHTML = statsOutput;
+  //   console.log(statsOutput)
+  // });  
 }
 
 
